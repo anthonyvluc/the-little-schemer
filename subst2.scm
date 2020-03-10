0210471943136771741cacd257c789849ee24d49
Anthony Luc <anthonyvluc@gmail.com>
2020-03-10 10:38:57 -0400

    complete? subst2

---
+++ subst2.scm
@@ -0,0 +1,6 @@
+(define subst2
+ (lambda (new o1 o2 lat)
+  (cond
+   ((null? lat) (quote ()))
+   ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+   (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
